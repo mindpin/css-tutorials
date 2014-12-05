44626cf0f0afafb91b5e25dfa37a62a43d596287
fushang318 <fushang318@gmail.com>
2014-12-05 17:05:02 +0800

    change css_tutorial fields

--- app/controllers/css_tutorials_controller.rb
+++ app/controllers/css_tutorials_controller.rb
@@ -8,7 +8,7 @@ class CssTutorialsController < ApplicationController
   def create
     css_tutorial_attr = params
       .require(:css_tutorial)
-      .permit(:title, :rule, :tips)
+      .permit(:title, :tid, :html, :css, :check_data, :tips_data)
     CssTutorial.create!(css_tutorial_attr)
     redirect_to :action => :index
   end
--- app/models/css_tutorial.rb
+++ app/models/css_tutorial.rb
@@ -2,9 +2,12 @@ class CssTutorial
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :title,  type: String
-  field :rule,   type: String
-  field :tips,   type: String
+  field :title,      type: String
+  field :tid,        type: String
+  field :html,       type: String
+  field :css,        type: String
+  field :check_data, type: String
+  field :tips_data,  type: String
 
   has_one :minicourse, :class_name => "EduGameMap::Minicourse"
 end
